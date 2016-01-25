9e082d58127092a3ad2eeb1037ad4bb1bb4a1b0c
Kevin Barabash <kevinb7@gmail.com>
2016-01-25 00:41:23 -0800

    initial implementation of the keyboard

--- src/button.js
+++ src/button.js
@@ -1,3 +1,52 @@
-/**
- * Created by kevin on 2016-01-24.
- */
+const React = require('react');
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: false
+        }
+    }
+
+    static defaultProps = {
+        color: '#099',
+        activeColor: '#066'
+    };
+
+    touchStart = () => {
+        console.log(this.props.children);
+        this.setState({ active: true });
+    };
+
+    touchEnd = () => {
+        this.setState({ active: false });
+    };
+
+    render() {
+        const {color, activeColor} = this.props;
+
+        const style = {
+            display: 'inline-block',
+            width: 47.5,
+            height: 32,
+            backgroundColor: this.state.active ? activeColor : color,
+            color: 'white',
+            lineHeight: '32px',
+            textAlign: 'center',
+            borderRadius: 4,
+            marginLeft: 5,
+            fontFamily: 'helvetica-light'
+        };
+
+        return <div
+            style={style}
+            onTouchStart={this.touchStart}
+            onTouchEnd={this.touchEnd}
+        >
+            {this.props.children}
+        </div>;
+    }
+}
+
+module.exports = Button;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class App extends React.Component {
-    render() {
-        return <div>
-            <h1>Hello, world</h1>
-        </div>;
-    }
-}
+const App = require('./app.js');
 
 const container = document.getElementById('app-container');
 ReactDOM.render(<App/>, container);
