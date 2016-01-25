3e7169ff42b387ece91b10ff109a847f3902ad12
Kevin Barabash <kevinb7@gmail.com>
2016-01-25 00:39:34 -0800

    get hot loading working properly

---
+++ src/button.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kevin on 2016-01-24.
+ */
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,27 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
     output: {
         path: path.join(__dirname, 'build'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
-        ]
+        filename: 'main.js',
+        publicPath: '/build/'
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            {
+                test: /\.js$/,
+                loader: 'react-hot',
+                include: path.join(__dirname, 'src')
             }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            compress: {
-                warnings: false
-            }
-        })
-    ]
+        ]
+    }
 };
