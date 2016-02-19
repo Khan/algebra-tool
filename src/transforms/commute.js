9e9164d838a6ade237586b62eceb3d85df6e20d6
Kevin Barabash <kevinb7@gmail.com>
2016-02-18 23:10:22 -0800

    fix issue with commute after refactoring Expression and Product

--- src/transforms/commute.js
+++ src/transforms/commute.js
@@ -3,10 +3,10 @@ function canTransform(selections) {
     let selection = selections[0];
 
     if (selection.length === 1 && ['Expression', 'Product'].includes(selection.first.type)) {
-        selection = selection.first;
+        selection = selection.first.children;
     }
-    if (selection.children && selection.children.length === 3) {
-        const [first, operator, ] = selection.children;
+    if (selection && selection.length === 3) {
+        const [first, operator, ] = selection;
 
         if (operator.operator === '-') {
             return false;
@@ -24,9 +24,9 @@ function doTransform(selections) {
         let selection = selections[0];
 
         if (selection.length === 1 && ['Expression', 'Product'].includes(selection.first.type)) {
-            selection = selection.first;
+            selection = selection.first.children;
         }
-        const { first, last } = selection.children;
+        const { first, last } = selection;
         const operator = first.next;
         const parent = operator.parent;
         parent.remove(first);
