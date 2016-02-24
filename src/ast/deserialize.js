00c91ee5c33660c425f57ab870487a4547d1bfeb
Kevin Barabash <kevinb7@gmail.com>
2016-02-24 13:59:48 -0800

    implement deserialize method for jsonified AST nodes

---
+++ src/ast/deserialize.js
@@ -0,0 +1,23 @@
+import * as ast from '../ast';
+import { generateId } from '../ast/node';
+import List from '../ast/list';
+
+
+function deserialize(json, unique = false) {
+    var obj = Object.create(ast[json.type].prototype);
+    for (const [key, value] of Object.entries(json)) {
+        if (Array.isArray(value)) {
+            obj[key] = new List(obj, ...value.map(deserialize, unique));
+        } else if (typeof value === "object") {
+            obj[key] = deserialize(value, unique);
+        } else {
+            obj[key] = value;
+        }
+    }
+    if (unique) {
+        obj.id = generateId();
+    }
+    return obj;
+}
+
+export { deserialize as default };
