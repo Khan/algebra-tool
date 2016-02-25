c7e2ddde77101ea82da3706bcb0b0ca779983bd1
James Irwin <james@khanacademy.org>
2016-02-25 15:23:17 -0800

    Port over rewrite as division
    Test Plan:
    http://localhost:3000/?start=(2x+2)/4
    Select the whole thing and choose "Change to multiplication"

--- src/transforms.js
+++ src/transforms.js
@@ -17,6 +17,7 @@ module.exports = {
     prime_factorization: require('./transforms/prime-factorization.js'),
     remove_parens: require('./transforms/remove-parens.js'),
     replace_selection: require('./transforms/replace-selection.js'),
+    rewrite_as_division: require('./transforms/rewrite-as-division.js'),
     rewrite_as_subtraction: require('./transforms/rewrite-as-subtraction'),
     rewrite_subtraction: require('./transforms/rewrite-subtraction'),
     swap_sides: require('./transforms/swap-sides'),
