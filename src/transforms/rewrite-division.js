30d14a9f4d0ef2376cdab07e750b8f65bd9567ae
Kevin Barabash <kevinb7@gmail.com>
2016-03-05 21:10:30 -0800

    update 'change to division' to handle division by fractions better

--- src/transforms/rewrite-division.js
+++ src/transforms/rewrite-division.js
@@ -23,13 +23,56 @@ function doTransform(selections) {
     const node = selection.first;
     const parent = node.parent;
 
-    parent.replace(
-        node,
-        mul(
-            node.numerator.clone(),
-            div(new Literal(1), node.denominator.clone())
-        )
-    );
+    const removeOnes = false;
+
+    if (node.denominator.type === 'Fraction') {
+        if (removeOnes && node.numerator.type === 'Literal' && parseFloat(node.numerator.value) === 1) {
+            if (removeOnes && node.denominator.numerator.type === 'Literal' && parseFloat(node.denominator.numerator.value) === 1) {
+                parent.replace(
+                    node,
+                    node.denominator.denominator.clone()
+                );
+            } else {
+                parent.replace(
+                    node,
+                    div(node.denominator.denominator.clone(), node.denominator.numerator.clone())
+                );
+            }
+        } else {
+            if (removeOnes && node.denominator.numerator.type === 'Literal' && parseFloat(node.denominator.numerator.value) === 1) {
+                parent.replace(
+                    node,
+                    mul(
+                        node.numerator.clone(),
+                        node.denominator.denominator.clone()
+                    )
+                );
+            } else {
+                parent.replace(
+                    node,
+                    mul(
+                        node.numerator.clone(),
+                        div(node.denominator.denominator.clone(), node.denominator.numerator.clone())
+                    )
+                );
+            }
+        }
+    } else {
+        if (removeOnes && node.numerator.type === 'Literal' && parseFloat(node.numerator.value) === 1) {
+            parent.replace(
+                node,
+                div(new Literal(1), node.denominator.clone())
+            );
+        } else {
+            parent.replace(
+                node,
+                mul(
+                    node.numerator.clone(),
+                    div(new Literal(1), node.denominator.clone())
+                )
+            );
+        }
+    }
 
     if (parent.type === 'Product') {
         removeExtraProductParens(parent);
